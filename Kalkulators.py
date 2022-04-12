9dcfb4c64b9422ffb7aab7898840046966993861
usacka1 <uusacka@gmail.com>
2022-04-12 09:32:57 +0300

    Stundas darbs

--- Kalkulators.py
+++ Kalkulators.py
@@ -3,26 +3,51 @@ from tkinter import*
 mansLogs = Tk()
 mansLogs.title("Kalkulators")
 # mansLogs.geometry("300x300")
-# izveido pogas
-btn0 = Button(mansLogs, text="0", padx="40", pady="20")
-btn1 = Button(mansLogs, text="1", padx="40", pady="20")
-btn2 = Button(mansLogs, text="2", padx="40", pady="20")
-btn3 = Button(mansLogs, text="3", padx="40", pady="20")
-btn4 = Button(mansLogs, text="4", padx="40", pady="20")
-btn5 = Button(mansLogs, text="5", padx="40", pady="20")
-btn6 = Button(mansLogs, text="6", padx="40", pady="20")
-btn7 = Button(mansLogs, text="7", padx="40", pady="20")
-btn8 = Button(mansLogs, text="8", padx="40", pady="20")
-btn9 = Button(mansLogs, text="9", padx="40", pady="20")
 
-btnSas = Button(mansLogs, text="+", padx="40", pady="20")
-btnAtn = Button(mansLogs, text="-", padx="40", pady="20")
-btnReiz = Button(mansLogs, text="*", padx="40", pady="20")
-btnDal = Button(mansLogs, text="/", padx="40", pady="20")
+def btnClick(number):
+    current = e.get()
+    # nolasa esošo skaitli
+    e.delete(0, END)
+    # nodzēš iepriekšējo
+    newNumber = str(current) + str(number)
+    e.insert(0, newNumber)
+    # ievieto displejā
+    return 0
+    # ja nav ko atgriezt, tad raksta ruturn 0
+
+def btnCommand(command):
+    global num1
+    # jāiegaumē skaitis un darbiba
+    global mathOp
+    mathOp = command
+    num1 = int(e.get())
+    e.delete(0, END)
+    return 0
+
+# izveido pogas
+e = Entry(mansLogs, width = 15, font=("Arial Black", 20))
+
+btn0 = Button(mansLogs, text="0", padx="40", pady="20", command = lambda:btnClick(0))
+btn1 = Button(mansLogs, text="1", padx="40", pady="20", command = lambda:btnClick(1))
+btn2 = Button(mansLogs, text="2", padx="40", pady="20", command = lambda:btnClick(2))
+btn3 = Button(mansLogs, text="3", padx="40", pady="20", command = lambda:btnClick(3))
+btn4 = Button(mansLogs, text="4", padx="40", pady="20", command = lambda:btnClick(4))
+btn5 = Button(mansLogs, text="5", padx="40", pady="20", command = lambda:btnClick(5))
+btn6 = Button(mansLogs, text="6", padx="40", pady="20", command = lambda:btnClick(6))
+btn7 = Button(mansLogs, text="7", padx="40", pady="20", command = lambda:btnClick(7))
+btn8 = Button(mansLogs, text="8", padx="40", pady="20", command = lambda:btnClick(8))
+btn9 = Button(mansLogs, text="9", padx="40", pady="20", command = lambda:btnClick(9))
+
+btnSas = Button(mansLogs, text="+", padx="40", pady="20", command = lambda:btnCommand("+"))
+btnAtn = Button(mansLogs, text="-", padx="40", pady="20", command = lambda:btnCommand("-"))
+btnReiz = Button(mansLogs, text="*", padx="40", pady="20", command = lambda:btnCommand("*"))
+btnDal = Button(mansLogs, text="/", padx="40", pady="20", command = lambda:btnCommand("/"))
 btnVien = Button(mansLogs, text="=", padx="40", pady="20")
 btnDel = Button(mansLogs, text="Del", padx="40", pady="20")
 
 # definē novietojumu
+e.grid(row=0, column=0, columnspan=4)
+
 btn7.grid(row=1, column=0)
 btn8.grid(row=1, column=1)
 btn9.grid(row=1, column=2)
