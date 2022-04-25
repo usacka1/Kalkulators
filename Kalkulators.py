24c6813052cc9415f58ed5575473d65c7230a281
usacka1 <uusacka@gmail.com>
2022-04-25 13:45:58 +0300

    Stundas darbs

--- Kalkulators.py
+++ Kalkulators.py
@@ -1,3 +1,4 @@
+from ast import operator
 from tkinter import*
 from math import*
 
@@ -26,7 +27,6 @@ def btnCommand(command):
     return 0
 
 def vienads():
-    global num2
     num2 = int(e.get())
     result = 0
     if mathOp == "+":
@@ -36,7 +36,7 @@ def vienads():
     elif mathOp == "*":
         result = num1 * num2
     elif mathOp == "/":
-        result == num1/num2
+        result = num1 / num2
     else:
         result = 0
     e.delete(0, END)
@@ -49,34 +49,82 @@ def clear():
     mathOp = ""
     return 0
 
+def kvadrSakne():
+    global operator
+    global num1
+    global mathOp
+    num1 = (int(e.get()))
+    num1 = sqrt(num1)
+    e.delete(0, END)
+    e.insert(0, num1)
+    return 0
+
+def kapinasana():
+    global operator
+    global num1
+    global mathOp
+    num1 = (int(e.get()))
+    num1 = num1 * num1
+    e.delete(0, END)
+    e.insert(0, num1)
+    return 0
+
+def procenti():
+    global operator
+    global num1
+    global mathOp
+    num1 = (float(e.get()))
+    num1 = "{:.0%}". format(num1)
+    e.delete(0, END)
+    e.insert(0, num1)
+    return 0
+
+def logaritms():
+    global operator
+    global num1
+    global mathOp
+    num1 = (int(e.get()))
+    num1 = log(num1, 10)
+    # pie bāzes 10
+    e.delete(0, END)
+    e.insert(0, num1)
+    return 0
+
 # izveido pogas
-e = Entry(mansLogs, width = 15, font=("Arial Black", 20))
+e = Entry(mansLogs, width = 15, bd = 20, font=("Arial Black", 20))
+# logs, kurā ievada skaitļus
 
-btn0 = Button(mansLogs, text="0", padx="40", pady="20", command = lambda:btnClick(0))
-btn1 = Button(mansLogs, text="1", padx="40", pady="20", command = lambda:btnClick(1))
-btn2 = Button(mansLogs, text="2", padx="40", pady="20", command = lambda:btnClick(2))
-btn3 = Button(mansLogs, text="3", padx="40", pady="20", command = lambda:btnClick(3))
-btn4 = Button(mansLogs, text="4", padx="40", pady="20", command = lambda:btnClick(4))
-btn5 = Button(mansLogs, text="5", padx="40", pady="20", command = lambda:btnClick(5))
-btn6 = Button(mansLogs, text="6", padx="40", pady="20", command = lambda:btnClick(6))
-btn7 = Button(mansLogs, text="7", padx="40", pady="20", command = lambda:btnClick(7))
-btn8 = Button(mansLogs, text="8", padx="40", pady="20", command = lambda:btnClick(8))
-btn9 = Button(mansLogs, text="9", padx="40", pady="20", command = lambda:btnClick(9))
+btn0 = Button(mansLogs, text="0", padx="30", pady="20", bd = 5, command = lambda:btnClick(0))
+btn1 = Button(mansLogs, text="1", padx="30", pady="20", bd = 5, command = lambda:btnClick(1))
+btn2 = Button(mansLogs, text="2", padx="30", pady="20", bd = 5, command = lambda:btnClick(2))
+btn3 = Button(mansLogs, text="3", padx="30", pady="20", bd = 5, command = lambda:btnClick(3))
+btn4 = Button(mansLogs, text="4", padx="30", pady="20", bd = 5, command = lambda:btnClick(4))
+btn5 = Button(mansLogs, text="5", padx="30", pady="20", bd = 5, command = lambda:btnClick(5))
+btn6 = Button(mansLogs, text="6", padx="30", pady="20", bd = 5, command = lambda:btnClick(6))
+btn7 = Button(mansLogs, text="7", padx="30", pady="20", bd = 5, command = lambda:btnClick(7))
+btn8 = Button(mansLogs, text="8", padx="30", pady="20", bd = 5, command = lambda:btnClick(8))
+btn9 = Button(mansLogs, text="9", padx="30", pady="20", bd = 5, command = lambda:btnClick(9))
 
-btnSas = Button(mansLogs, text="+", padx="40", pady="20", command = lambda:btnCommand("+"))
-btnAtn = Button(mansLogs, text="-", padx="40", pady="20", command = lambda:btnCommand("-"))
-btnReiz = Button(mansLogs, text="*", padx="40", pady="20", command = lambda:btnCommand("*"))
-btnDal = Button(mansLogs, text="/", padx="40", pady="20", command = lambda:btnCommand("/"))
-btnVien = Button(mansLogs, text="=", padx="40", pady="20", command = vienads)
-btnDel = Button(mansLogs, text="Del", padx="40", pady="20", command = clear)
+btnSas = Button(mansLogs, text="+", padx="30", pady="20", bd = 5, command = lambda:btnCommand("+"))
+btnAtn = Button(mansLogs, text="-", padx="30", pady="20", bd = 5, command = lambda:btnCommand("-"))
+btnReiz = Button(mansLogs, text="*", padx="30", pady="20", bd = 5, command = lambda:btnCommand("*"))
+btnDal = Button(mansLogs, text="/", padx="30", pady="20", bd = 5, command = lambda:btnCommand("/"))
+#kvadrātsakne, kāpināšana, porcenti, ???------------------------------------------------------------------------------------
+btnKvadr = Button(mansLogs, text="√", padx="30", pady="20", bd = 5, command = kvadrSakne)
+btnKapin = Button(mansLogs, text="x²", padx="30", pady="20", bd = 5, command = kapinasana)
+btnProc = Button(mansLogs, text="%", padx="30", pady="20", bd = 5, command = procenti)
+btnLogar = Button(mansLogs, text="log", padx="25", pady="20", bd = 5, command = logaritms)
+#---------------------------------------------------------------------------------------------------------------------------
+btnVien = Button(mansLogs, text="=", padx="30", pady="20", bd = 5, command = vienads)
+btnDel = Button(mansLogs, text="C", padx="30", pady="20", bd = 5, command = clear)
 
 # definē novietojumu
 e.grid(row=0, column=0, columnspan=4)
 
-btn7.grid(row=1, column=0)
-btn8.grid(row=1, column=1)
-btn9.grid(row=1, column=2)
-btn4.grid(row=2, column=0)
+btnDel.grid(row=1, column=0)
+btnKapin.grid(row=1, column=1)
+btnKvadr.grid(row=1, column=2)
+btn7.grid(row=2, column=0)
 btn5.grid(row=2, column=1)
 btn6.grid(row=2, column=2)
 btn1.grid(row=3, column=0)
@@ -84,11 +132,17 @@ btn2.grid(row=3, column=1)
 btn3.grid(row=3, column=2)
 btn0.grid(row=4, column=0)
 
-btnDal.grid(row=1, column=3)
+btnProc.grid(row=1, column=3)
 btnReiz.grid(row=2, column=3)
 btnSas.grid(row=3, column=3)
 btnAtn.grid(row=4, column=3)
-btnDel.grid(row=4, column=1)
+#----------------------------------------------------------------------------------------------------------------------------
+btn4.grid(row=5, column=0)
+btn8.grid(row=5, column=1)
+btnDal.grid(row=5, column=2)
+btnLogar.grid(row=5, column=3)
+#----------------------------------------------------------------------------------------------------------------------------
+btn7.grid(row=4, column=1)
 btnVien.grid(row=4, column=2)
 
 mansLogs.mainloop()
